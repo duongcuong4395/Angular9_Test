02328bdfd40905329574d16a408c30998e66ddf6
cuong dang <dangduongcuong@gmail.com>
2020-05-10 22:11:40 +0700

    Ceate Form and test submit

---
+++ testAngular9/projects/tv-show-rating/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TvRatingFormComponent } from './tv-rating-form/tv-rating-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TvRatingFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
